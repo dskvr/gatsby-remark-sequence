6353fecdd44f737764d01741f1750e8e81c97665
Sandwich <299465+dskvr@users.noreply.github.com>
2019-11-20 12:41:50 +0100

    Too verbose for large websites
    The console.info here was bloating logs

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const visit = require('unist-util-visit')
 module.exports = ({ markdownAST }, { language = 'sequence', theme = 'default' } = {}) => {
   visit(markdownAST, 'code', node => {
     let lang = (node.lang || '').toLowerCase()
-    console.info(`lang is ${lang}`)
     if (lang === language) {
       node.type = 'html'
       node.value = `<div class="sequence">${node.value}</div>`
